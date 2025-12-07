298b22e41db26ee69157745f10262f346878b253
Rio <rio@graf.co.id>
2025-12-07 19:51:14 +0700

    update

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -7,6 +7,7 @@ import ReactPlayer from 'react-player'
 import { useEffect, useState } from 'react';
 import { AxiosClient } from '@/api-client/AxiosClient';
 import { IDRFormatter } from '@/utility';
+import { DollarSign } from 'lucide-react';
 
 
 export const Route = createFileRoute('/')({
@@ -26,46 +27,66 @@ export const Route = createFileRoute('/')({
 
     const user = UserSession.getUser();
     return (
-      <Layout className='flex flex-col gap-4 text-xl'>
+      <Layout className='flex flex-col gap-4 text-xl w-full'>
         { !user.id && <div className='flex flex-col gap-2 items-center text-center bg-zinc-50 py-4'>
           <div className='text-orange-500'>
             Login untuk memberikan Donasi
           </div>
           <GoogleLoginButton onSuccess={() => window.location.href = '/main'} />
         </div> }
-        <div className='flex flex-col gap-2 my-4'>
-          <div className='text-2xl'>
-            Total Donasi Terkumpul
+        <div className='flex lg:flex-row justify-between items-center flex-col gap-0 pb-4'>
+          <div className='flex flex-col gap-2 my-4'>
+            <div className='text-2xl'>
+              Total Donasi Terkumpul
+            </div>
+            <div className='text-4xl font-extrabold'>
+              { loading && <Spinner /> }
+              { !loading && IDRFormatter.format(total_donasi) }
+            </div>
           </div>
-          <div className='text-4xl font-extrabold'>
-            { loading && <Spinner /> }
-            { !loading && IDRFormatter.format(total_donasi) }
-          </div>
+          { user.id && <Button 
+            href='/main'
+            color='primary'
+            startContent={<DollarSign size={16} />}
+            variant='bordered'
+            className='whitespace-pre-wrap text-center !h-12 text-lg'
+            as={Link}>
+            Berikan Donasi
+          </Button>}
         </div>
         <div className='grid grid-cols-1 gap-3 lg:grid-cols-2 lg:gap-4'>
           <Button 
             href='/donatur'
             color='warning'
-            className='whitespace-pre-wrap text-center !h-12'
+            className='whitespace-pre-wrap text-center !h-12 text-lg'
             as={Link}>
             Lihat List Donatur
           </Button>
           <Button 
             href='/laporan'
             color='warning'
-            className='whitespace-pre-wrap text-center !h-12'
+            className='whitespace-pre-wrap text-center !h-12 text-lg'
             as={Link}>
             Lihat Laporan Penggunaan Donasi
           </Button>
         </div>
-        <div className='h-80 lg:h-120'>
-          <ReactPlayer 
-            width={'100%'}
-            height={'100%'}
-            src='https://www.youtube.com/watch?v=fSy8NIl5--c' />
-        </div>
         <div className='text-zinc-600'>
           Ribuan orang di Sumatra telah kehilangan rumah akibat banjir besar dan tanah longsor yang menghancurkan - banyak yang berduka atas anggota keluarga atau masih mencari mereka yang hilang. Jalan dan jembatan tidak dapat dilalui, listrik dan jaringan seluler terputus. Situasi pasokan semakin kritis dari hari ke hari, dan persediaan makanan semakin menipis.
+        </div>
+        <div className='flex items-center gap-2 h-90 w-full'>
+          <div className='flex-1 h-full'>
+            <img 
+              className='w-full h-full object-cover'
+              src={'/IMG-20251207-WA0013.webp'} />
+          </div>
+          <div className='flex-1 h-full'>
+            <img 
+              className='w-full h-full object-cover'
+              src={'/IMG-20251207-WA0014.webp'} />
+          </div>
+        </div>
+        <div className='text-zinc-600'>
+          Setiap donasi membantu memberikan perlindungan, makanan, dan harapan bagi para korban. Bersama-sama, kita dapat menyelamatkan nyawa.
         </div>
         <div className='h-80 lg:h-120'>
           <ReactPlayer 
@@ -74,10 +95,13 @@ export const Route = createFileRoute('/')({
             src='https://www.youtube.com/watch?v=iaKnOVjvm0Y' />
         </div>
         <div className='text-zinc-600'>
-          Setiap donasi membantu memberikan perlindungan, makanan, dan harapan bagi para korban. Bersama-sama, kita dapat menyelamatkan nyawa.
-        </div>
-        <div className='text-zinc-600'>
           Dukungan sangat dibutuhkan - untuk pembangunan kembali rumah dan infrastruktur yang hancur, untuk menyediakan makanan dan air minum bersih, serta layanan medis dan pakaian. Jaringan listrik dan komunikasi juga perlu dipulihkan agar bantuan dapat dikoordinasikan dan masyarakat dapat kembali terhubung dengan keluarga mereka. Setiap bentuk bantuan berkontribusi dalam memberikan harapan, serta membantu masyarakat di sana kembali melangkah sedikit demi sedikit menuju kehidupan yang aman.
+        </div>
+        <div className='h-80 lg:h-120'>
+          <ReactPlayer 
+            width={'100%'}
+            height={'100%'}
+            src='https://www.youtube.com/watch?v=fSy8NIl5--c' />
         </div>
         { !user.id && <div className='flex flex-col gap-2 items-center text-center bg-zinc-50 py-4'>
           <div className='text-orange-500'>
@@ -89,14 +113,14 @@ export const Route = createFileRoute('/')({
           <Button 
             href='/donatur'
             color='warning'
-            className='whitespace-pre-wrap text-center !h-12'
+            className='whitespace-pre-wrap text-center !h-12 text-lg'
             as={Link}>
             Lihat List Donatur
           </Button>
           <Button 
             href='/laporan'
             color='warning'
-            className='whitespace-pre-wrap text-center !h-12'
+            className='whitespace-pre-wrap text-center !h-12 text-lg'
             as={Link}>
             Lihat Laporan Penggunaan Donasi
           </Button>
--- web/src/routes/laporan.tsx
+++ web/src/routes/laporan.tsx
@@ -1,11 +1,17 @@
 import { Layout } from '@/components/Layout';
 import { createFileRoute } from '@tanstack/react-router'
+import { Info } from 'lucide-react';
 
 export const Route = createFileRoute('/laporan')({
   component() {
     return (
-      <Layout className='flex flex-col gap-8'>
-        TBD
+      <Layout className='flex flex-col gap-8 flex-1'>
+        <div className='text-rose-500 flex items-center gap-2'>
+          <Info size={18} />
+          <div>
+            Laporan akan diposting disini setiap kali donasi diberikan.
+          </div>
+        </div>
       </Layout>
     );
   }
