d384eaab59f12dffed86f4bd3df6f8dbb298498a
Rio <riochr17@gmail.com>
2026-01-14 22:05:24 +0700

    update

--- implementation/loginWithGoogle.ts
+++ implementation/loginWithGoogle.ts
@@ -5,22 +5,27 @@ import { verifyGoogleLogin } from "../verify-google-login";
 import { Donatur } from "../types/model/table/Donatur";
 
 export const loginWithGoogle: T_loginWithGoogle = async req => {
-  const data = await verifyGoogleLogin(req.body.google_access_token);
-  if (!data || !data?.email) {
-    throw new Error(`user account doesnt have email`);
-  }
+  try {
+    const data = await verifyGoogleLogin(req.body.google_access_token);
+    if (!data || !data?.email) {
+      throw new Error(`user account doesnt have email`);
+    }
 
-  let user = await Donatur.findOneBy({ email: data.email });
-  if (!user) {
-    user = new Donatur();
-    user.nama = data.name || '<google account>';
-    user.email = data.email;
-    user.google_user_id = data.sub;
-    await user.save();
-  }
+    let user = await Donatur.findOneBy({ email: data.email });
+    if (!user) {
+      user = new Donatur();
+      user.nama = data.name || '<google account>';
+      user.email = data.email;
+      user.google_user_id = data.sub;
+      await user.save();
+    }
 
-  return {
-    token: signJWT(user.id),
-    user
+    return {
+      token: signJWT(user.id),
+      user
+    }
+  } catch (err) {
+    console.log(err);
+    throw err;
   }
 }
